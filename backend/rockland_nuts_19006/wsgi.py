3baf325c05fb33f5347f85f4ec615b69b87c0e95
Crowdbotics <team@crowdbotics.com>
2020-07-19 01:52:07 +0000

    Initial commit.

---
+++ backend/rockland_nuts_19006/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rockland_nuts_19006 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rockland_nuts_19006.settings')
+
+application = get_wsgi_application()
